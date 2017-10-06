e5f80c8f18aae54c3bdc2bee766465553e202813
Bridget Almas <balmsa@gmail.com>
2017-10-06 15:52:15 -0400

    load the other dictionaries

--- exist-startup.sh
+++ exist-startup.sh
@@ -7,6 +7,14 @@
 # restore the apps
 /opt/exist/bin/backup.sh -u admin -p admin -r /tmp/lexsvc.zip
 /opt/exist/bin/backup.sh -u admin -p admin -r /tmp/stg.zip
+/opt/exist/bin/backup.sh -u admin -p admin -r /tmp/ls.zip
+/opt/exist/bin/backup.sh -u admin -p admin -r /tmp/lsj.zip
+/opt/exist/bin/backup.sh -u admin -p admin -r /tmp/aut.zip
+/opt/exist/bin/backup.sh -u admin -p admin -r /tmp/as.zip
+/opt/exist/bin/backup.sh -u admin -p admin -r /tmp/dod.zip
+/opt/exist/bin/backup.sh -u admin -p admin -r /tmp/ml.zip
+/opt/exist/bin/backup.sh -u admin -p admin -r /tmp/lan.zip
+/opt/exist/bin/backup.sh -u admin -p admin -r /tmp/sal.zip
 
 # make sure exist is good and stopped and stale pid file gone
 /opt/exist/tools/wrapper/bin/exist.sh stop
